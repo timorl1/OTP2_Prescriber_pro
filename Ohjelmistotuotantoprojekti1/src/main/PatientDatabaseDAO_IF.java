0f215f44f0a8ef344bffc4b44eb05fd9036f04bb
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-02-06 16:03:54 +0200

    PatientDatabaseDAO_IF about tehty

--- Ohjelmistotuotantoprojekti1/src/main/PatientDatabaseDAO_IF.java
+++ Ohjelmistotuotantoprojekti1/src/main/PatientDatabaseDAO_IF.java
@@ -10,5 +10,12 @@ package main;
  * @author joosiika
  */
 public interface PatientDatabaseDAO_IF {
-    public abstract boolean createConnection(PatientDBParameter parameters);
+    //Creates the connection to the Patient database
+    public abstract boolean createConnection(String url, String username, String password);
+    //Reads all field names from the Patient database and returns them as array of strings
+    public abstract String[] getDBFieldNames();
+    //Accesses the software's database and reads from the patientdbparameter-table and constructs the PatientDBParamater-object
+    public abstract PatientDBParameter readPatientDBParameter();
+    //Accesses the software's database and writes a PatientDBParameter-object into the patientdbparameter-table
+    public abstract boolean writePatientDBParameter();
 }
