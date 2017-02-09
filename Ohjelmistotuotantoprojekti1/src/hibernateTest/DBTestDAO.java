8b8ee43740c2528f7378bed0d6e91e788b6ffa2f
Timo <Timo@DESKTOP-45SI9OJ.wlan.metropolia.fi>
2017-02-09 14:45:28 +0200

    libraries added

--- Ohjelmistotuotantoprojekti1/src/hibernateTest/DBTestDAO.java
+++ Ohjelmistotuotantoprojekti1/src/hibernateTest/DBTestDAO.java
@@ -1,9 +1,7 @@
 package hibernateTest;
-import java.util.ArrayList;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
