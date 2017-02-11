a3a90e805b7b70e4934ff460043b1f0dc24cea3b
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-02-11 11:12:54 +0200

    PatientDatabaseDAO nimetty DatabaseDAO:ksi ja lisätty muutama metodi: - getDatabases (palauttaa palvelimella olevien tietokantojen nimet) - getTables (palauttaa tietokannassa olevien taulujen nimet) Ottaa parametrina Properties-olion, johon talletetaan tietokannan parametrit ja tiedoston nimen, johon Properties-olio tallennetaan.

--- Ohjelmistotuotantoprojekti1/src/consoletests/PatientDatabaseSetupTestRun.java
+++ Ohjelmistotuotantoprojekti1/src/consoletests/PatientDatabaseSetupTestRun.java
@@ -7,7 +7,7 @@ package consoletests;
 
 import java.util.ArrayList;
 import java.util.Properties;
-import main.PatientDatabaseDAO;
+import main.DatabaseDAO;
 
 /**
  *
@@ -19,8 +19,12 @@ public class PatientDatabaseSetupTestRun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        PatientDatabaseDAO PDD = new PatientDatabaseDAO();
-        Properties PDP = PDD.readPatientDBProperties();
+        Properties PDP = new Properties();
+        DatabaseDAO PDD = new DatabaseDAO(PDP, "db.properties");
+        Properties DDP = new Properties();
+        DatabaseDAO DDD = new DatabaseDAO(DDP, "db.properties");
+        PDP = PDD.readDBProperties();
+        DDP = DDD.readDBProperties();
         String fieldName;
         
         if (PDP.isEmpty()) {
@@ -30,11 +34,25 @@ public class PatientDatabaseSetupTestRun {
             PDP.setProperty("username", Reader.readLine());
             System.out.println("Tietokannan salasana: ");
             PDP.setProperty("password", Reader.readLine());
-            System.out.println("Tietotaulun nimi: ");
-            PDP.setProperty("tableName", Reader.readLine());
-            PDD.writePatientDBProperties(PDP);
+            PDD.writeDBProperties("Patient DB Settings");
             //Luo tietokantayhteys ja hae kenttien nimet
-            ArrayList<String>fieldNames = PDD.getDBFieldNames(PDD.createConnection(PDP.getProperty("url"), PDP.getProperty("username"), PDP.getProperty("password")));
+            ArrayList<String>fieldNames = PDD.getDatabases();
+            
+            System.out.println("Valitse tietokanta: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            PDP.setProperty("db", fieldNames.get(Reader.readInt()-1));
+            
+            fieldNames = PDD.getTables();
+            
+            System.out.println("Valitse tietotaulu: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            PDP.setProperty("table", fieldNames.get(Reader.readInt()-1));
+            
+            fieldNames = PDD.getDBFieldNames();
             
             System.out.println("Valitse sosiaaliturvatunnuksen kentän nimi: ");
             for (int i = 0; i < fieldNames.size(); i++) {
@@ -72,16 +90,64 @@ public class PatientDatabaseSetupTestRun {
             }
             PDP.setProperty("height", fieldNames.get(Reader.readInt()-1));
             
-            System.out.println("Valitse allergiakentän nimi: ");
+            PDD.writeDBProperties("Patient DB Settings");
+        }
+        if (DDP.get("diagnoseID") == null) {
+            System.out.println("Tietokannan URL: ");
+            DDP.setProperty("url", Reader.readLine());
+            System.out.println("Tietokannan käyttäjätunnus: ");
+            DDP.setProperty("username", Reader.readLine());
+            System.out.println("Tietokannan salasana: ");
+            DDP.setProperty("password", Reader.readLine());
+            DDD.writeDBProperties("Diagnose DB Settings");
+            //Luo tietokantayhteys ja hae kenttien nimet
+            ArrayList<String>fieldNames = DDD.getDatabases();
+            
+            System.out.println("Valitse tietokanta: ");
             for (int i = 0; i < fieldNames.size(); i++) {
                 System.out.println(i+1 + ". " + fieldNames.get(i));
             }
-            PDP.setProperty("allergies", fieldNames.get(Reader.readInt()-1));
+            DDP.setProperty("db", fieldNames.get(Reader.readInt()-1));
             
-            PDD.writePatientDBProperties(PDP);
+            fieldNames = DDD.getTables();
+            
+            System.out.println("Valitse tietotaulu: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            DDP.setProperty("table", fieldNames.get(Reader.readInt()-1));
+            
+            fieldNames = DDD.getDBFieldNames();
+            
+            System.out.println("Valitse tunniste kentän nimi: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            DDP.setProperty("diagnoseID", fieldNames.get(Reader.readInt()-1));
+            
+            System.out.println("Valitse sairaustunnistekentän nimi: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            DDP.setProperty("diseaseID", fieldNames.get(Reader.readInt()-1));
+            
+            System.out.println("Valitse potilastunnistekentän nimi: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            DDP.setProperty("patientID", fieldNames.get(Reader.readInt()-1));
+            
+            System.out.println("Valitse lääkäritunnistekentän nimi: ");
+            for (int i = 0; i < fieldNames.size(); i++) {
+                System.out.println(i+1 + ". " + fieldNames.get(i));
+            }
+            DDP.setProperty("doctorID", fieldNames.get(Reader.readInt()-1));
+            
+            DDD.writeDBProperties("Diagnose DB Settings");
         }
         else {
             System.out.println(PDP);
+            System.out.println(DDP);
         }
     }
     
