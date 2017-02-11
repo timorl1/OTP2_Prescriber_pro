3f2b11d9903d41cba0eff1188949eb04d650dd40
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-02-11 11:15:06 +0200

    PatientDatabaseSetupTestRun-testiohjelmaa korjattu

--- Ohjelmistotuotantoprojekti1/src/consoletests/PatientDatabaseSetupTestRun.java
+++ Ohjelmistotuotantoprojekti1/src/consoletests/PatientDatabaseSetupTestRun.java
@@ -21,10 +21,7 @@ public class PatientDatabaseSetupTestRun {
     public static void main(String[] args) {
         Properties PDP = new Properties();
         DatabaseDAO PDD = new DatabaseDAO(PDP, "db.properties");
-        Properties DDP = new Properties();
-        DatabaseDAO DDD = new DatabaseDAO(DDP, "db.properties");
         PDP = PDD.readDBProperties();
-        DDP = DDD.readDBProperties();
         String fieldName;
         
         if (PDP.isEmpty()) {
@@ -92,62 +89,8 @@ public class PatientDatabaseSetupTestRun {
             
             PDD.writeDBProperties("Patient DB Settings");
         }
-        if (DDP.get("diagnoseID") == null) {
-            System.out.println("Tietokannan URL: ");
-            DDP.setProperty("url", Reader.readLine());
-            System.out.println("Tietokannan käyttäjätunnus: ");
-            DDP.setProperty("username", Reader.readLine());
-            System.out.println("Tietokannan salasana: ");
-            DDP.setProperty("password", Reader.readLine());
-            DDD.writeDBProperties("Diagnose DB Settings");
-            //Luo tietokantayhteys ja hae kenttien nimet
-            ArrayList<String>fieldNames = DDD.getDatabases();
-            
-            System.out.println("Valitse tietokanta: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("db", fieldNames.get(Reader.readInt()-1));
-            
-            fieldNames = DDD.getTables();
-            
-            System.out.println("Valitse tietotaulu: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("table", fieldNames.get(Reader.readInt()-1));
-            
-            fieldNames = DDD.getDBFieldNames();
-            
-            System.out.println("Valitse tunniste kentän nimi: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("diagnoseID", fieldNames.get(Reader.readInt()-1));
-            
-            System.out.println("Valitse sairaustunnistekentän nimi: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("diseaseID", fieldNames.get(Reader.readInt()-1));
-            
-            System.out.println("Valitse potilastunnistekentän nimi: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("patientID", fieldNames.get(Reader.readInt()-1));
-            
-            System.out.println("Valitse lääkäritunnistekentän nimi: ");
-            for (int i = 0; i < fieldNames.size(); i++) {
-                System.out.println(i+1 + ". " + fieldNames.get(i));
-            }
-            DDP.setProperty("doctorID", fieldNames.get(Reader.readInt()-1));
-            
-            DDD.writeDBProperties("Diagnose DB Settings");
-        }
         else {
             System.out.println(PDP);
-            System.out.println(DDP);
         }
     }
     
