f166a5e482bf322b657142775f4f1e65aad0396c
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-02-12 13:37:04 +0200

    Diagnose ja Diagnoses luokat tehty.

--- Ohjelmistotuotantoprojekti1/src/main/Diagnose.java
+++ Ohjelmistotuotantoprojekti1/src/main/Diagnose.java
@@ -1,14 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
+
+import java.sql.Timestamp;
 
 /**
  *
  * @author joosiika
  */
 public class Diagnose {
+    private int id;
+    private String diseaseID;
+    private String epicrisis;
+    private Timestamp creationDate;
+    private Timestamp resolutionDate;
+    private Patient patient;
+    private Doctor doctor;
+
+    public Diagnose() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDiseaseID() {
+        return diseaseID;
+    }
+
+    public void setDiseaseID(String diseaseID) {
+        this.diseaseID = diseaseID;
+    }
+
+    public String getEpicrisis() {
+        return epicrisis;
+    }
+
+    public void setEpicrisis(String epicrisis) {
+        this.epicrisis = epicrisis;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Timestamp getResolutionDate() {
+        return resolutionDate;
+    }
+
+    public void setResolutionDate(Timestamp resolutionDate) {
+        this.resolutionDate = resolutionDate;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
     
 }
--- Ohjelmistotuotantoprojekti1/src/main/Diagnoses.java
+++ Ohjelmistotuotantoprojekti1/src/main/Diagnoses.java
@@ -5,10 +5,38 @@
  */
 package main;
 
+import java.util.List;
+
 /**
  *
  * @author joosiika
  */
 public class Diagnoses {
+    private List<Diagnose> diagnoselist;
+
+    public Diagnoses(List diagnoselist) {
+        this.diagnoselist = diagnoselist;
+    }
     
+    public void addDiagnose(Diagnose diagnose) {
+        this.diagnoselist.add(diagnose);
+    }
+    
+    public Diagnose getDiagnose(int id) {
+        if (diagnoselist.indexOf(id) != -1) {
+            return diagnoselist.get(diagnoselist.indexOf(id));
+        }
+        return null;
+    }
+    
+    public List getCollection() {
+        return this.diagnoselist;
+    }
+    
+    public Diagnose forEach() {
+        for (Diagnose diagnose : this.diagnoselist) {
+            return diagnose;
+        }
+        return null;
+    }
 }
