88704f84ed1e64b9780f104aaa4239a28086ccb0
Timo <Timo@DESKTOP-45SI9OJ.wlan.metropolia.fi>
2017-02-14 12:18:52 +0200

    Pohja UserDAO luokalle ja UserDAO_IF rajapinta

---
+++ Ohjelmistotuotantoprojekti1/src/dao/UserDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.User;
+
+/**
+ *
+ * @author Timo
+ */
+public class UserDAO implements UserDAO_IF {
+
+    @Override
+    public void updateUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Ohjelmistotuotantoprojekti1/src/dao/UserDAO_IF.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.User;
+
+/**
+ *
+ * @author Timo
+ */
+public interface UserDAO_IF {
+    public abstract void updateUser(User user);
+    
+}
