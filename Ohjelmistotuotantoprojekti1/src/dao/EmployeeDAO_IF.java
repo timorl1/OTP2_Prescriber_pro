5f0d664d898061512bf71eb33f8a15509b5ef67c
Paula <Paula@Aatos.wlan.metropolia.fi>
2017-03-02 20:22:16 +0200

    Toteutettu emnloyeeDAO_IF luokka

---
+++ Ohjelmistotuotantoprojekti1/src/dao/EmployeeDAO_IF.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import model.Employee;
+import java.util.List;
+
+/**
+ *
+ * @author Paula
+ */
+public interface EmployeeDAO_IF {
+    public abstract Employee readEmployee(int userID);
+    public abstract List<Employee> readEmployees();
+    
+    /*
+    public abstract List<Employee>getEmployees();
+    public abstract Employee getEmployee(int userID); */
+    
+}
