800fd399a3be6e2b4263c642a0b793476b139846
Johanna <Johanna@johannaPC.wlan.metropolia.fi>
2017-03-07 08:59:10 +0200

    Message-luokka tehty

--- Ohjelmistotuotantoprojekti1/src/model/Message.java
+++ Ohjelmistotuotantoprojekti1/src/model/Message.java
@@ -1,9 +1,62 @@
 package model;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author joosiika
  */
+@Entity(name="message")
+@Table(name="message")
 public class Message {
+    
+    @Id
+    @Column(name="MessageID")
+    private int messageID;
+    @Column(name="message")
+    private String message;
+    @Column(name="date")
+    private Date date;
+    
+    public Message(){}
+    
+    public Message(int id, String message, Date d){
+        this.messageID = id;
+        this.message = message;
+        this.date = d;
+    }
+
+    public int getMessageID() {
+        return messageID;
+    }
+
+    public void setMessageID(int messageID) {
+        this.messageID = messageID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message: " + "messageID= " + messageID + ", message= " + message + ", date= " + date ;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
     
 }
