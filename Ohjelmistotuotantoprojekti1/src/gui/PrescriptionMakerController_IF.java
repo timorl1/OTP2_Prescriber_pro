996868e48cf93a62b686f47e947b5f5ba2d54e44
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-03-15 14:32:27 +0200

    JavaDoccia tehty

--- Ohjelmistotuotantoprojekti1/src/gui/PrescriptionMakerController_IF.java
+++ Ohjelmistotuotantoprojekti1/src/gui/PrescriptionMakerController_IF.java
@@ -6,20 +6,24 @@
 package gui;
 
 /**
- *
+ * Interface for implementing controller class for PrescriptionMaker
  * @author joosiika
  */
 public interface PrescriptionMakerController_IF {
 
     /**
-     *
-     * @return
+     * Method to get the optimal dose for a patient.
+     * @return the optimal dose of a certain drug for a certain patient as a double value
      */
     public abstract double getOptimalDose();
 
     /**
-     *
+     * Method to check the dosage level of a drug.
      */
     public abstract void checkDose();
+    
+    /**
+     * Method to check if the drug contains allergens not suitable for the patient.
+     */
     public abstract void checkAllergens();
 }
