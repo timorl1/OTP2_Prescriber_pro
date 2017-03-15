0191ddb314cec746dd6068294aea8cc030de3058
Johanna <Johanna@johannaPC.wlan.metropolia.fi>
2017-03-15 15:49:03 +0200

    User_IF ja DrugResources_IF javadocjutut tehty

--- Ohjelmistotuotantoprojekti1/src/model/User_IF.java
+++ Ohjelmistotuotantoprojekti1/src/model/User_IF.java
@@ -6,108 +6,107 @@
 package model;
 
 import java.util.List;
-import javax.persistence.*;
 
 /**
- *
+ * Interface defines methods for users
  * @author Timo
  */
 
 public interface User_IF {
 
     /**
-     *
-     * @param username
+     * Sets username to a user-object
+     * @param username user's username
      */
     public abstract void setUsername(String username);
 
     /**
-     *
-     * @return
+     * Gets user-object's username
+     * @return username
      */
     public abstract String getUsername();
 
     /**
-     *
-     * @param userID
+     * Sets userID to a user-object
+     * @param userID user's id
      */
     public abstract void setUserID(int userID);
 
     /**
-     *
-     * @return
+     * Gets user-object's userID
+     * @return user's id
      */
     public abstract int getUserID();
 
     /**
-     *
-     * @param password
+     * Sets password to a user-object
+     * @param password user's password
      */
     public abstract void setPassword(String password);
 
     /**
-     *
-     * @return
+     * Gets user-object's password
+     * @return user's password
      */
     public abstract String getPassword();
 
     /**
-     *
-     * @param email
+     * Sets email to a user-object
+     * @param email user's email
      */
     public abstract void setEmail(String email);
 
     /**
-     *
-     * @return
+     * Gets user-object's email
+     * @return user's email
      */
     public abstract String getEmail();
 
     /**
-     *
-     * @param privileges
+     * Sets privileges to a user-object
+     * @param privileges user's privileges
      */
     public abstract void setUsertype(int privileges);
 
     /**
-     *
-     * @return
+     * Gets user-object's usertype
+     * @return user's usertype
      */
     public abstract int getUsertype();
 
     /**
-     *
-     * @param firstName
+     * Sets first name to a user-object
+     * @param firstName user's first name
      */
     public abstract void setFirstName(String firstName);
 
     /**
-     *
-     * @return
+     * Gets user-object's fist name
+     * @return user's first name
      */
     public abstract String getFirstName();
 
     /**
-     *
-     * @param lastName
+     * Sets last name to a user-object
+     * @param lastName user's last name
      */
     public abstract void setLastName(String lastName);
 
     /**
-     *
-     * @return
+     * Gets user-object's last name
+     * @return user's last name
      */
     public abstract String getLastName();
 
     /**
-     *
-     * @return
+     * Gets user-object's sent messages
+     * @return list of message-objects
      */
     public abstract List<Message> getSentMessages();
 
     /**
-     *
-     * @return
+     * Gets user-object's received messages
+     * @return list of message-objects
      */
     public abstract List<Message> getReceivedMessages();
     
