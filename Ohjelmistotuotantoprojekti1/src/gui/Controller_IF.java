ad5ebbf75c7ff921f5b927046f15c24d8ee93be0
Joonas Siikavirta <joonas.siikavirta@metropolia.fi>
2017-03-16 13:21:56 +0200

    Tehty javadoc maingui_if, controller_if

--- Ohjelmistotuotantoprojekti1/src/gui/Controller_IF.java
+++ Ohjelmistotuotantoprojekti1/src/gui/Controller_IF.java
@@ -17,112 +17,144 @@ import model.User_IF;
 public interface Controller_IF {
 
     /**
-     *
-     * @param username
-     * @param password
+     * Method to set the current user.
+     * @param username username entered by the user
+     * @param password password entered by the user
      */
     public abstract void login(String username, String password);
 
     /**
-     *
-     * @return
+     * Method to serve patients from the backend to the view.
+     * @return list of patients
      */
     public abstract List<Patient> getPatients();
+    /**
+     * Method to serve patient's diagnoses from the backend to the view.
+     * @return list of diagnoses
+     */
     public abstract List<Diagnose> getPatientDiagnoses();
+    
+    /**
+     * Method to serve patient's prescriptions from the backend to the view.
+     * @return list of prescriptions
+     */
     public abstract List<Prescription> getPatientPrescriptions();
+    
+    /**
+     * Method to serve drugs from the backend to the view.
+     * @return list of drugs
+     */
     public abstract List<Drug> getDrugs();
 
     /**
-     *
+     * Method to serve drug details from the backend to the view.
      */
     public abstract void getDrugDetails();
+    
+    /**
+     * Method to serve prescriptions made by current user from the backend to the view.
+     * @return list of prescriptions
+     */
     public abstract List<Prescription> getDoctorPrescriptions();
+    
+    /**
+     * Method to serve prescription details from the backend to the view.
+     */
     public abstract void getPrescriptionDetails();
 
     /**
-     *
+     * Method to serve diagnose details from the backend to the view.
      */
     public abstract void getDiagnoseDetails();
 
     /**
-     *
-     * @return
+     * Method to serve current user's received messages from the backend to the view.
+     * @return list of received messages
      */
     public abstract List<Message> getReceivedMessages();
 
     /**
-     *
-     * @return
+     * Method to serve current user's sent messages from the backend to the view.
+     * @return list of sent messages
      */
     public abstract List<Message> getSentMessages();
 
     /**
-     *
+     * Method to serve message details from the backend to the view.
      */
     public abstract void getMessageDetails();
 
     /**
-     *
-     * @return
+     * Method to serve users from the backend to the view.
+     * @return list of users
      */
     public abstract List<User_IF> getUsers();
 
     /**
-     *
+     * Method to serve user details from the backend to the view.
      */
     public abstract void getUserDetails();
 
     /**
-     *
-     * @return
+     * Method to serve employees from the backend to the view.
+     * @return list of employees
      */
     public abstract List<Employee> getEmployees();
 
     /**
-     *
+     * Method to serve employees details from the backend to the view.
      */
     public abstract void getEmployeeDetails();
 
     /**
-     *
-     * @return
+     * Method to serve list of databases from the backend to the view.
+     * @return list of database names
      */
     public abstract List<String> getDatabases();
 
     /**
-     *
+     * Method to create a new prescription in the backend and set it into the prescription form.
      */
     public abstract void createNewPrescription();
 
     /**
-     *
-     * @return
+     * Method to get a prescription from view and serve it to the backend for writing to database.
+     * @return success state of the save or update from the backend
      */
     public abstract boolean savePrescription();
 
     /**
-     *
-     * @param user
+     * Method to serve a user from the view to the backend for setting user privileges to locked state.
+     * @param user to be locked
      */
     public abstract void lockUser(User_IF user);
 
     /**
-     *
-     * @param user
+     * Method to serve a user from the view to the backend for restoring user's privileges.
+     * @param user to be unlocked
      */
     public abstract void setUserPriviledges(User_IF user);
 
     /**
-     *
-     * @return
+     * Method to serve a message from the view to the backend to be written to the database.
+     * @return success state from the backend
      */
     public abstract boolean saveMessage();
 
     /**
-     *
+     * Method to create a new message in the backend and set it into the message form.
      */
     public abstract void createNewMessage();
+    
+    /**
+     * Method to create a new user in the backend and set it into the user form.
+     */
     public abstract void createNewUser();
+    
+    /**
+     * Method to get a user from user form view and serve it to the backend for writing to database.
+     * @return success state of the save or update from the backend
+     */
     public abstract boolean saveUser();
     
 }
--- Ohjelmistotuotantoprojekti1/src/gui/MainGUI_IF.java
+++ Ohjelmistotuotantoprojekti1/src/gui/MainGUI_IF.java
@@ -11,201 +11,210 @@ import model.User;
 import model.User_IF;
 
 /**
- *
+ * Interface for Main View JavaFX element with anchor pane as root
  * @author joosiika
  */
 public interface MainGUI_IF {
 
     /**
-     *
+     * Method to set the login view.
      */
     public abstract void setLogin();
 
     /**
-     *
+     * Method to set a login failed message.
      */
     public abstract void setLoginFailed();
 
     /**
-     *
+     * Method to set an access denied message.
      */
     public abstract void setAccessDenied();
 
     /**
-     *
+     * method to set a sidebar main element.
      */
     public abstract void setSideBar();
 
     /**
-     *
+     * Method to set the patient list view.
      */
     public abstract void setPatientList();
 
     /**
-     *
+     * Method to set the drug list view.
      */
     public abstract void setDrugList();
 
     /**
-     *
+     * Method to set the prescription list view.
      */
     public abstract void setPrescriptionList();
 
     /**
-     *
+     * Method to set the received message list view.
      */
     public abstract void setReceivedMessageList();
 
     /**
-     *
+     * Method to set the sent message list view.
      */
     public abstract void setSentMessageList();
 
     /**
-     *
+     * Method to set the user list view.
      */
     public abstract void setUserList();
 
     /**
-     *
+     * Method to set the employee list view.
      */
     public abstract void setEmployeeList();
 
     /**
-     *
+     * Method to set the database list view.
      */
     public abstract void setDatabaseList();
 
     /**
-     *
-     * @param prescription
+     * Method to set the prescription creation or update view
+     * @param prescription the prescription to be manipulated by the user
      */
     public abstract void setPrescriptionForm(Prescription prescription);
 
     /**
-     *
-     * @param message
+     * Method to set the message creation view
+     * @param message the message to be manipulated by the user
      */
     public abstract void setMessageForm(Message message);
 
     /**
-     *
+     * Method to set the additional buttons for users with privileges to create prescriptions.
      */
     public abstract void setPrescriptionTools();
 
     /**
-     *
-     * @param patient
+     * Method to set patient details to the view.
+     * @param patient the patient whose details the user wants to view
      */
     public abstract void setPatientDetails(Patient patient);
 
     /**
-     *
-     * @param list
+     * Method to set the patient's diagnose list to the view.
+     * @param list patient's diagnose list
      */
     public abstract void setPatientDiagnoses(List<Diagnose> list);
 
     /**
-     *
-     * @param list
+     * Method to set the patient's prescription list to the view.
+     * @param list patient's prescription list
      */
     public abstract void setPatientPrescriptions(List<Prescription> list);
 
     /**
-     *
-     * @param prescription
+     * Method to set prescription's details to the view.
+     * @param prescription prescription to be shown in detail
      */
     public abstract void setPrescriptionDetails(Prescription prescription);
 
     /**
-     *
-     * @param diagnose
+     * Method to set diagnose's details to the view.
+     * @param diagnose diagnose to be shown in detail
      */
     public abstract void setDiagnoseDetails(Diagnose diagnose);
 
     /**
-     *
-     * @param user
+     * Method to set user's details to the view.
+     * @param user user to be shown in detail
      */
     public abstract void setUserDetails(User_IF user);
 
     /**
-     *
-     * @param employee
+     * Method to set employees's details to the view.
+     * @param employee employee to be shown in detail
      */
     public abstract void setEmployeeDetails(Employee employee);
 
     /**
-     *
-     * @return
+     * Method to get the selected patient from the view.
+     * @return patient selected in patient list view
      */
     public abstract Patient getSelectedPatient();
 
     /**
-     *
-     * @return
+     * Method to get the selected prescription from the view.
+     * @return prescription selected in prescription list view
      */
     public abstract Prescription getSelectedPrescription();
 
     /**
-     *
-     * @return
+     * Method to get the selected diagnose from the list view.
+     * @return diagnose selected in diagnose list view
      */
     public abstract Diagnose getSelectedDiagnose();
 
     /**
-     *
-     * @return
+     * Method to get the selected drug from the list view.
+     * @return drug selected in drug list view
      */
     public abstract Drug getSelectedDrug();
 
     /**
-     *
-     * @return
+     * Method to get the selected user from the list view.
+     * @return user selected in user list view
      */
     public abstract User_IF getSelectedUser();
 
     /**
-     *
-     * @return
+     * Method to get the selected drug from the list view.
+     * @return employee selected in employee list view
      */
     public abstract Employee getSelectedEmployee();
 
     /**
-     *
-     * @return
+     * Method to get the prescription manipulated by the prescription form view.
+     * @return prescription updated by the user
      */
     public abstract Prescription getPrescriptionForm();
 
     /**
-     *
-     * @param drug
+     * Method to set drug's details to the view.
+     * @param drug to be shown in detail
      */
     public abstract void setDrugDetails(Drug drug);
 
     /**
-     *
-     * @param message
+     * Method to set message's details to the view.
+     * @param message message to be shown in detail
      */
     public abstract void setMessageDetails(Message message);
 
     /**
-     *
-     * @return
+     * Method to get the selected message from list view.
+     * @return message selected in the message list view
      */
     public abstract Message getSelectedMessage();
 
     /**
-     *
-     * @return
+     * Method to get the message manipulated by the message form view.
+     * @return message updated by the message form
      */
     public abstract Message getMessageForm();
 
     /**
-     *
-     * @param drug
+     * Method to set the user creation or update form to the view.
+     * @param user user to be manipulated by the form
      */
     public abstract void setUserForm(User_IF user);
+    
+    /**
+     * Method to set the administrator tools to the view.
+     */
     public abstract void setUserTools();
+    
+    /**
+     * Method to get the user manipulated by the user form view.
+     * @return user updated by the user form view.
+     */
     public abstract User_IF getUserForm();
 }
